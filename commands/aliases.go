4198e56ca38dc5ac187529669f19a9bda1bb96ac
Szymon Nowicki <sznowicki@gmail.com>
2020-04-08 23:07:41 +0200

    dkr cleanup

--- commands/aliases.go
+++ commands/aliases.go
@@ -18,18 +18,42 @@ func handleJumpIntoTerminal(mode string, shArgs []string) {
 
 func handleKillAll() {
 	ids := string(ReturnCommand("docker", []string{"ps", "-q"}))
-
 	idsArr := strings.Split(strings.Trim(ids, "\n"), "\n")
 
 	if idsArr[0] == "" {
 		fmt.Print("There's nothing to kill.")
-		os.Exit(1)
+		os.Exit(0)
 	}
 
 	for _, element := range idsArr {
 		ExecCommand("docker", []string{"kill", element})
 	}
 
+	os.Exit(0)
+}
+
+func handleCleanup() {
+	ids := string(ReturnCommand("docker", []string{"ps", "-aq"}))
+	idsArr := strings.Split(strings.Trim(ids, "\n"), "\n")
+
+	if idsArr[0] != "" {
+		for _, element := range idsArr {
+			ExecCommand("docker", []string{"rm", element})
+		}
+	} else {
+		fmt.Print("No containers to remove.\n")
+	}
+
+	imagesIds := string(ReturnCommand("docker", []string{"images", "-q"}))
+	imagesIdsArr := strings.Split(strings.Trim(imagesIds, "\n"), "\n")
+
+	if imagesIdsArr[0] != "" {
+		for _, element := range imagesIdsArr {
+			ExecCommand("docker", []string{"rmi", "-f", element})
+		}
+	} else {
+		fmt.Print("No volumes to remove.\n")
+	}
 	os.Exit(0)
 }
 
@@ -56,5 +80,7 @@ func DetectAndCallAliases() {
 		handleJumpIntoTerminal("/bin/bash", rest)
 	case "killall":
 		handleKillAll()
+	case "cleanup":
+		handleCleanup()
 	}
 }
